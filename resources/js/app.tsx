5775c6aa847d4c2374cd876c5aca1e613fdcd68f
Groovy Mark <116895994+shootcannon@users.noreply.github.com>
2026-05-23 20:45:11 +0700

    Update app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -4,5 +4,5 @@ const appName = import.meta.env.VITE_APP_NAME || 'Fufas Markets';
 
 createInertiaApp({
     title: (title) => (title ? `${title} · ${appName}` : appName),
-    progress: false, // replaced with custom FufasProgress bar
+    progress: false, 
 });
